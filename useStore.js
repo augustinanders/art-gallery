e8e62cd39c4d13ab52a387cb4d51cd17f3dd51dd
Augustin Anders <augustinanders@gmail.com>
2023-06-16 11:52:37 +0200

    initialise new state

--- useStore.js
+++ useStore.js
@@ -3,6 +3,22 @@ import { create } from "zustand";
 const useStore = create((set) => ({
   artPieces: [],
   setArtPieces: (data) => set({ artPieces: data }),
+
+  artPiecesInfo: [],
+  setIsFavourite: (slug) => set((state) => {
+    const artPiecesInfo = state.artPiecesInfo.map((artPiece) => {
+      if (artPiece.slug === slug) {
+        return {
+          ...artPiece,
+          isFavourite: !artPiece.isFavourite,
+        };
+      }
+      return artPiece;
+    });
+    return { artPiecesInfo };
+  }),
+  
+  }
 }));
 
 export default useStore;
