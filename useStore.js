fb248596d71df6f066a6df456c2c31b248726373
Augustin Anders <augustinanders@gmail.com>
2023-06-27 10:27:45 +0200

    fix useStore

--- useStore.js
+++ useStore.js
@@ -5,20 +5,19 @@ const useStore = create((set) => ({
   setArtPieces: (data) => set({ artPieces: data }),
 
   artPiecesInfo: [],
-  setIsFavourite: (slug) => set((state) => {
-    const artPiecesInfo = state.artPiecesInfo.map((artPiece) => {
-      if (artPiece.slug === slug) {
-        return {
-          ...artPiece,
-          isFavourite: !artPiece.isFavourite,
-        };
-      }
-      return artPiece;
-    });
-    return { artPiecesInfo };
-  }),
-  
-  }
+  setIsFavourite: (slug) =>
+    set((state) => {
+      const artPiecesInfo = state.artPiecesInfo.map((artPiece) => {
+        if (artPiece.slug === slug) {
+          return {
+            ...artPiece,
+            isFavourite: !artPiece.isFavourite,
+          };
+        }
+        return artPiece;
+      });
+      return { artPiecesInfo };
+    }),
 }));
 
 export default useStore;
